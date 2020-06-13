75608a12f0a43de495167e9e49468f9fc77b890f
harvestcore <aagomezies@gmail.com>
2020-06-13 16:19:57 +0200

    Cleanup

--- app/src/main/java/com/agm/boatme/MarinePoints.java
+++ app/src/main/java/com/agm/boatme/MarinePoints.java
@@ -51,7 +51,6 @@ public class MarinePoints {
                 e.printStackTrace();
             }
         }
-        System.out.println(this.points.size());
     }
 
     public ArrayList<InterestPoint> getPoints() {
